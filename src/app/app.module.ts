019f6beaf5a2ef98fec53e1b03a10cf608d56dd5
yoshix3 <y.nara.2000+github@gmail.com>
2022-05-28 23:32:07 +0900

    app.motule.tsにコンポーネントを追加。htmlファイルも追加。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BoardComponent } from './board/board.component';
+import { GameComponent } from './game/game.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    BoardComponent,
+    GameComponent,
+    SquareComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
-  styleUrls: ['./square.component.css']
+  styleUrls: ['./square.component.css'],
 })
 export class SquareComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
