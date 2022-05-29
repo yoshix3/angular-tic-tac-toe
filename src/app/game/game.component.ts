5ae41f559d3a174e091b5eefac4a1e2fb4b6d479
yoshix3 <y.nara.2000+github@gmail.com>
2022-05-29 11:56:55 +0900

    gameコンポーネント-3,4

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -24,8 +24,49 @@ export class GameComponent implements OnInit {
     this.stepNumber = num;
     this.xIsNext = num % 2 === 0;
   }
-  onClicked(event: number) {}
+  onClicked(event: number) {
+    const his = this.history.slice(0, this.stepNumber + 1);
+    const sq = his[his.length - 1].squares.slice();
+    if (this.calculateWinner(sq) || sq[event]) {
+      return;
+    }
+    sq[event] = this.xIsNext ? 'X' : 'O';
+    this.history = this.history.concat([{ squares: sq }]);
+    this.stepNumber = this.history.length - 1;
+
+    const winner = this.calculateWinner(this.history[this.stepNumber].squares);
+
+    if (winner) {
+      this.status = 'Winner: ' + winner;
+    } else {
+      this.xIsNext = !this.xIsNext;
+      this.status = 'Next player: ' + (this.xIsNext ? 'X' : 'O');
+    }
+  }
   trackFn(index: any, move: squareType) {
     return index;
+  }
+  private calculateWinner(squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 }
