4f081ccecca3a7e244f15ae44c59e834b76dbb3e
yoshix3 <y.nara.2000@gmail.com>
2022-05-29 11:28:57 +0900

    boardコンポーネント

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.css'],
 })
 export class BoardComponent implements OnInit {
+  @Input() squares: string[];
+  @Output() clicked = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  onClicked(event: number) {
+    this.clicked.emit(event);
   }
-
 }
