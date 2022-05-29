a1fa1330453eb77d008f629513fe2089b70d3e2d
yoshix3 <y.nara.2000+github@gmail.com>
2022-05-29 11:47:40 +0900

    gameコンポーネント-1

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+type squareType = {
+  squares: string[];
+};
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
+  history: squareType[] = [{ squares: Array(9).fill(null) }];
+  status: string = '';
+  stepNumber: number = 0;
+  xIsNext: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  jumpTo(num: number) {
+    this.stepNumber = num;
+    this.xIsNext = num % 2 === 0;
   }
-
+  onClicked(event: number) {}
+  trackFn(index: any, move: squareType) {
+    return index;
+  }
 }
